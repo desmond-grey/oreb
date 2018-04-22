590497e77d7b7579f29e99758e772577ea76bc64
Gary Affonso <gary@teahousesoftware.com>
2018-04-22 12:07:16 -0700

    Initial Guidance spinners (onItemSelected still needs to be done)

--- app/src/main/java/com/teahousesoftware/oreb/guidance/GuidanceFragment.kt
+++ app/src/main/java/com/teahousesoftware/oreb/guidance/GuidanceFragment.kt
@@ -1,14 +1,25 @@
 package com.teahousesoftware.oreb.guidance
 
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import com.teahousesoftware.oreb.OrebViewModel
 import com.teahousesoftware.oreb.R
+import com.teahousesoftware.oreb.shared.model.music.TheoreticalNote
 
 
-class GuidanceFragment : Fragment() {
+class GuidanceFragment : Fragment(), AdapterView.OnItemSelectedListener {
+    val DEFAULT_KEY = TheoreticalNote.C
+    val DEFAULT_SCALE_NAME = "Major"                // TODO: do not hardcode string
+
+    private lateinit var orebViewModel: OrebViewModel
+
     private var pageNum: Int = 0
     private var title: String? = null
 
@@ -27,12 +38,63 @@ class GuidanceFragment : Fragment() {
         super.onCreate(savedInstanceState)
         pageNum = arguments.getInt("pageNum", 0)
         title = arguments.getString("title")
+        orebViewModel = ViewModelProviders.of(this.activity).get(OrebViewModel::class.java)
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater!!.inflate(R.layout.fragment_guidance, container, false)
-        return view
+        val layout = inflater!!.inflate(R.layout.fragment_guidance, container, false)
+
+        // wire up the key spinner
+        val keySpinner = layout.findViewById(R.id.key_spinner) as Spinner
+        val keySpinnerAdapter = ArrayAdapter(
+                this.activity,
+                android.R.layout.simple_spinner_item,
+                TheoreticalNote.values().map { it.descriptiveName }
+        )
+        keySpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        keySpinner.adapter = keySpinnerAdapter
+        keySpinner.setOnItemSelectedListener(this)
+        keySpinner.setSelection(DEFAULT_KEY.ordinal)
+        
+        // wire up the scale spinner
+        val scaleSpinner = layout.findViewById(R.id.scale_spinner) as Spinner
+        val scaleSpinnerAdapter = ArrayAdapter(
+                this.activity,
+                android.R.layout.simple_spinner_item,
+                orebViewModel.scales.map { it.name }
+        )
+        scaleSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        scaleSpinner.adapter = scaleSpinnerAdapter
+        scaleSpinner.setOnItemSelectedListener(this)
+        scaleSpinner.setSelection(orebViewModel.scales.indexOfFirst { it.name == DEFAULT_SCALE_NAME })
+
+        return layout
     }
 
+    override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
+        when (parent.id) {
+            R.id.key_spinner -> consumeSpinnerEvent { keySpinnerItemSelected(parent.getItemAtPosition(pos) as CharSequence) }
+            R.id.scale_spinner -> consumeSpinnerEvent { scaleSpinnerItemSelected(parent.getItemAtPosition(pos) as CharSequence) }
+        }
+    }
+
+    inline fun consumeSpinnerEvent(f: () -> Unit): Boolean {
+        f()
+        return true
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        // TODO
+    }
+
+    private fun keySpinnerItemSelected(keyName: CharSequence) {
+        val newKey = TheoreticalNote.values().find { it.descriptiveName == keyName }!!
+        // TODO: update viewModel 
+    }
+
+    private fun scaleSpinnerItemSelected(scaleName: CharSequence) {
+        val newScale = this.orebViewModel.scales.find { it.name == scaleName }!!
+        // TODO: update viewModel 
+    }
 }
--- app/src/main/java/com/teahousesoftware/oreb/shared/model/music/TheoreticalNote.kt
+++ app/src/main/java/com/teahousesoftware/oreb/shared/model/music/TheoreticalNote.kt
@@ -1,16 +1,16 @@
 package com.teahousesoftware.oreb.shared.model.music
 
-enum class TheoreticalNote {
-    C,
-    C_SHARP_D_FLAT,
-    D,
-    D_SHARP_E_FLAT,
-    E,
-    F,
-    F_SHARP_G_FLAT,
-    G,
-    G_SHARP_A_FLAT,
-    A,
-    A_SHARP_B_FLAT,
-    B
+enum class TheoreticalNote(val descriptiveName: String) {
+    C("C"),
+    C_SHARP_D_FLAT("C#/Db"),
+    D("D"),
+    D_SHARP_E_FLAT("D#/Eb"),
+    E("E"),
+    F("F"),
+    F_SHARP_G_FLAT("F#/Gb"),
+    G("G"),
+    G_SHARP_A_FLAT("G#/Ab"),
+    A("A"),
+    A_SHARP_B_FLAT("A#/Bb"),
+    B("B")
 }
